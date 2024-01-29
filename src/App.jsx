24e9703bbf2b52d9763fff7e408d407fb1a7e6da
Rafael Ramos <junior07.rrn@hotmail.com>
2024-01-29 00:23:10 -0500

    Added globals styles and navigation bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./styles/App.scss";
+import "./styles/partials/_globals.scss";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import DashboardPage from "./pages/DashboardPage/DashboardPage";
+import Navbar from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <BrowserRouter>
+      <Navbar />
       <Routes>
-        <Route path="/" element={<DashboardPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
       </Routes>
     </BrowserRouter>
   );
