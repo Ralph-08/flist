3d3e76851efb941fed507fb42a23892148ced53e
Rafael Ramos <junior07.rrn@hotmail.com>
2024-01-30 18:32:27 -0500

    added weekly list component

--- src/pages/DashboardPage/DashboardPage.jsx
+++ src/pages/DashboardPage/DashboardPage.jsx
@@ -1,37 +1,15 @@
 import "./DashboardPage.scss";
-import Item from "../../components/Item/Item";
+
+import WeeklyList from "../../components/WeeklyList/WeeklyList";
 
 const DashboardPage = () => {
-
-
-  
   return (
     <>
       <section className="dashboard">
         <h1 className="dashboard__header">Dashboard</h1>
 
-        <h2 className="dashboard__subheader">Weekly List:</h2>
-        <section className="dashboard__card">
-
-          <section className="dashboard__container">
-            <ul className="subhead__list">
-              <li className="subhead__item">
-                <p className="subhead__header">Item:</p>
-              </li>
-              <li className="subhead__item">
-                <p className="subhead__header">Price:</p>
-              </li>
-            </ul>
-          </section>
-
-          <Item />
-          <Item />
-          <Item />
-          <Item />
-          <Item />
-
-
-        </section>
+        <WeeklyList />
+        <WeeklyList />
       </section>
     </>
   );
