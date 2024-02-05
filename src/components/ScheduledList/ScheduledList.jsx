41fc9eb89fe6c1125eefce0789f6101741e4e9dd
Rafael Ramos <junior07.rrn@hotmail.com>
2024-02-04 20:15:50 -0500

    Added dynamic list rendering with API

--- src/components/ScheduledList/ScheduledList.jsx
+++ src/components/ScheduledList/ScheduledList.jsx
@@ -1,8 +1,17 @@
 import Item from "../Item/Item";
 import editIcon from "../../assets/icons/edit-pencil.svg";
 import "./ScheduledList.scss";
+import EditListModal from "../EditListModal/EditListModal";
+import { useState } from "react";
 
-const ScheduledList = () => {
+const ScheduledList = ({ items }) => {
+  const [editModal, setEditModal] = useState(false);
+  const [scheduledDate, setScheduledDate] = useState(null);
+
+  const handleEditDisplay = () => {
+    !editModal ? setEditModal(true) : setEditModal(false);
+  };
+
   return (
     <section className="scheduled">
       <ul className="scheduled__list">
@@ -10,7 +19,10 @@ const ScheduledList = () => {
           <h2 className="scheduled__subheader">Scheduled List:</h2>
         </li>
         <li className="scheduled__item">
-          <button className="scheduled__button scheduled__button--secondary">
+          <button
+            onClick={handleEditDisplay}
+            className="scheduled__button scheduled__button--secondary"
+          >
             <img className="scheduled__add-icon" src={editIcon} alt="" />
             Edit
           </button>
@@ -27,26 +39,47 @@ const ScheduledList = () => {
             </li>
           </ul>
         </section>
-
-        <Item />
-        <Item />
+        {!items ? (
+          <h3>Loading...</h3>
+        ) : (
+          items.map((item) => {
+            return (
+              <Item
+                img={item.image}
+                price={item.price}
+                title={item.title}
+                rating={item.rating}
+                asin={item.asin}
+                key={item.asin}
+              />
+            );
+          })
+        )}
         <section className="list-info">
           <ul className="list-info__list">
             <li className="list-info__item">
               <p className="list-info__text">
-                <span className="bold">Scheduled for:</span> Every two weeks
+                <span className="bold">Scheduled for:</span> Every two Fridays
               </p>
               <p className="list-info__text">
-                <span className="bold">Next order due:</span> Friday, Feb 2
+                <span className="bold">Next order due:</span> Friday, Feb 16
               </p>
             </li>
             <li className="list-info__item">
               <p className="list-info__text">
-                <span className="bold">Total:</span> $25.78
+                <span className="bold">Total:</span> $7.99
               </p>
             </li>
           </ul>
         </section>
+        {editModal && (
+          <EditListModal
+            handleClose={handleEditDisplay}
+            scheduledDate={scheduledDate}
+            setScheduledDate={setScheduledDate}
+            itemsList={items}
+          />
+        )}
       </section>
     </section>
   );
--- src/pages/DashboardPage/DashboardPage.jsx
+++ src/pages/DashboardPage/DashboardPage.jsx
@@ -1,15 +1,37 @@
 import "./DashboardPage.scss";
 import ScheduledList from "../../components/ScheduledList/ScheduledList";
 import WeeklyList from "../../components/WeeklyList/WeeklyList";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const DashboardPage = () => {
+  const [editModal, setEditModal] = useState(false);
+  const [scheduledLists, setScheduledLists] = useState(null);
+
+  const handleEditDisplay = () => {
+    !editModal ? setEditModal(true) : setEditModal(false);
+  };
+
+  const getScheduledLists = async () => {
+    try {
+      const res = await axios.get("http://localhost:8080/lists");
+      setScheduledLists(res.data.filter((list) => !list.weekly_list));
+    } catch (err) {
+      console.log("Error getting scheduled lists: ", err);
+    }
+  };
+
+  useEffect(() => {
+    getScheduledLists();
+  }, []);
+
   return (
     <>
       <section className="dashboard">
         <h1 className="dashboard__header">Dashboard</h1>
-
-        <WeeklyList />
-        <ScheduledList />
+        <WeeklyList handleEditModal={handleEditDisplay} />
+        {scheduledLists &&
+          scheduledLists.map((list) => <ScheduledList items={list.items} />)}
       </section>
     </>
   );
