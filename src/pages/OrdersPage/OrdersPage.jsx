46b2647a400e37816116772be76dd11121291cfa
Rafael Ramos <junior07.rrn@hotmail.com>
2024-02-09 21:02:46 -0500

    Add OrdersPage and Order components

--- src/pages/OrdersPage/OrdersPage.jsx
+++ src/pages/OrdersPage/OrdersPage.jsx
@@ -1,7 +1,7 @@
 import "./OrdersPage.scss";
 import axios from "axios";
 import { useState, useEffect } from "react";
-import Item from "../../components/Item/Item";
+import Order from "../../components/Order/Order";
 
 const OrdersPage = () => {
   const [ordersList, setOrdersList] = useState(null);
@@ -24,26 +24,7 @@ const OrdersPage = () => {
       <h1 className="orders__header">Orders</h1>
       <section className="orders__container">
         {ordersList &&
-          ordersList.map((order) => (
-            <section className="order__card">
-              <h2 className="order_subheader">
-                {new Intl.DateTimeFormat("en-US", {
-                  year: "numeric",
-                  month: "long",
-                  day: "numeric",
-                }).format(order.order_date)}
-              </h2>
-              <section className="order__items-container">
-                {order.items.map((item) => (
-                  <Item
-                    img={item.image}
-                    title={item.title}
-                    price={item.price}
-                  />
-                ))}
-              </section>
-            </section>
-          ))}
+          ordersList.map((order) => <Order order={order} key={order._id} />)}
       </section>
     </section>
   );
